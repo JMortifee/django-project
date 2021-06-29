56f7004efdf9895ba7165a5beaf98ae1a6eae7a1
James Mortifee <james.mortifee@hotmail.co.uk>
2021-06-29 09:12:38 +0100

    stripe initial setup completed

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key' : 'pk_test_51J7bkAAkIYnOVpTZhjFjCpRButaRd5VKNS4Yu7k0thODm2azoigW1h6RchyIUd98UgPV0Nh1OemAM2b2HpQqdRsv00QowWGNKX','client_secret' :'test_key'
     }
 
     return render(request, template, context)
