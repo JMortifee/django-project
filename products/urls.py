2553d107065e88f0d2fec97e6bff525b5d4b3a7d
James Mortifee <james.mortifee@hotmail.co.uk>
2021-06-29 19:24:41 +0100

    added the add product and edit product templates and views

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.portfolio_years, name='portfolio_years'),
     path('media/', views.media, name='media'),
     path('all_works/', views.all_products, name='products'),
-    path('<product_id>/', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
